726375bd90462d6a5067ccb3553d08e12317eb55
Debjit Sarkar <sarkarbabudebjit@gmail.com>
2023-05-14 22:00:18 +0530

    homepage mods

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_chat/pages/loginpage.dart';
+import 'package:firebase_chat/pages/searchpage.dart';
 import 'package:firebase_chat/service/authprovider.dart';
 import 'package:firebase_chat/widgets/widgets.dart';
 import 'package:flutter/material.dart';
@@ -16,7 +17,29 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("HomePage")),
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          title: Text("Groups"),
+          centerTitle: true,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  nextPage(context: context, page: SearchPage());
+                },
+                icon: Icon(Icons.search_outlined))
+          ],
+        ),
+        drawer: Drawer(
+            child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          children: [
+            Icon(
+              Icons.account_circle_rounded,
+              size: 150,
+              color: Colors.grey[70],
+            )
+          ],
+        )),
         body: ElevatedButton(
             child: Text("Logout"),
             onPressed: () {
@@ -25,3 +48,5 @@ class _HomePageState extends State<HomePage> {
             }));
   }
 }
+
+//1:37
